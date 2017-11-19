1f2c2e66f0c5576f355d1a68d5f94e55f54ea8c6
katsew <y.katsew@gmail.com>
2017-11-19 23:25:37 +0900

    Update heartcatch

--- heartcatch/app.py
+++ heartcatch/app.py
@@ -1,38 +1,40 @@
 from flask import Flask, request, json, make_response
 import os
 from googleapiclient.discovery import build
-import pprint
+from googleapiclient.http import HttpError
 
 api = Flask(__name__)
 
 @api.route("/images", methods=['GET'])
 def images():
 
-    api_key = os.getenvb(b'API_KEY', str(''))
-    custom_engine_id = os.getenvb(b'CSE_ID', str(''))
+    api_key = os.getenvb(b'API_KEY', b'')
+    cse_id = os.getenvb(b'CSE_ID', b'')
 
-    default_query = os.getenvb(b'DEFAULT_QUERY', str('可愛い女の子'))
+    default_query = os.getenvb(b'DEFAULT_QUERY', str('可愛い女の子').encode('utf-8'))
     query = request.args.get('q', default_query)
 
-    svc = build("customsearch", "v1", developerKey=api_key)
-
+    svc = build("customsearch", "v1", developerKey=api_key.decode('utf-8'))
     try:
         res = svc.cse().list(
-            q=query,
-            cx=custom_engine_id,
+            q=query.decode('utf-8'),
+            cx=cse_id.decode('utf-8'),
             lr='lang_ja',
             searchType='image',
             start=1,
             num=10,
         ).execute()
-    except:
-        pprint.pprint(res)
-
-    return make_response(json.jsonify(res))
-
+        api.logger.info("Result: %s", res)
+        return make_response(json.jsonify(res))
+    except HttpError as err:
+        api.logger.error("Content: %s", err.content)
+        api.logger.error("Detail: %s", err.error_details)
+        api.logger.error("Response: %s", err.resp)
+        return make_response(err.content, 500)
 
 if __name__ == "__main__":
 
-    h = os.getenvb(b'API_HOST', str('0.0.0.0'))
-    p = os.getenvb(b'API_PORT', str('5000'))
-    api.run(host=h, port=p)
+    h = os.getenvb(b'API_HOST', str('localhost').encode('utf-8'))
+    p = os.getenvb(b'API_PORT', str('5000').encode('utf-8'))
+
+    api.run(host=h.decode('utf-8'), port=p.decode('utf-8'))
